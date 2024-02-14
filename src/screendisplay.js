ded6eaf62a92747613f191a4c9866a887a6272a3
Daniel <roderm99@gmail.com>
2024-02-14 15:54:51 -0500

    create css doc, and add intial html skeleton

--- src/screendisplay.js
+++ src/screendisplay.js
@@ -6,11 +6,9 @@ function ScreenController() {
 
     const createHomePage = () => {
 
-        const homeTitle = document.createElement('h1');
         const projectsListTitle = document.createElement('h2');
         const currentProjectTitle = document.createElement('h3');
 
-        homeTitle.textContent = "The Grand List of To Dos";
         projectsListTitle.textContent = "Projects";
         currentProjectTitle = "This Project"; //placeholder for now
 
