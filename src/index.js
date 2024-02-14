92636c726582950e5c05084371c699184f8f7fc7
Daniel <roderm99@gmail.com>
2024-02-14 17:34:16 -0500

    create addprojecttolist function in screendisplay.js to display projects in list on screen

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import { ProjectManager } from './project.js';
 import { ToDoManager } from './todos.js';
+import { createHomePage,
+    addProjectToList,
+    createThisProjectSection, } from './screendisplay.js';
 
 console.log("Big Dawgs STAY barkin");
+
+const container = document.querySelector('#container');
+container.appendChild(createHomePage());
 
 console.log(ToDoManager().getToDos());
 
 console.log(ProjectManager().getMyProjects());
+
+addProjectToList();
+
+
 
 
--- src/screendisplay.js
+++ src/screendisplay.js
@@ -2,24 +2,68 @@ import { ToDoManager } from "./todos";
 import { ProjectManager } from "./project"; 
 
 
-function ScreenController() {
+const createHomePage = () => {
 
-    const createHomePage = () => {
+    const homeContainer = document.createElement('div');
+    homeContainer.setAttribute('id', 'home-container');
 
-        const projectsListTitle = document.createElement('h2');
-        const currentProjectTitle = document.createElement('h3');
+    const projectListDiv = document.createElement('div');
+    const projectsListTitle = document.createElement('h2');
+    projectListDiv.setAttribute('id', 'proj-list-div');
+    projectsListTitle.setAttribute('id', 'proj-list-title');
+        
+    const currentProjectDiv = document.createElement('div');
+    const currentProjectTitle = document.createElement('h2');
+    currentProjectDiv.setAttribute('id', 'current-proj-div');
+    currentProjectTitle.setAttribute('id', 'current-proj-title');
+        
+    projectsListTitle.textContent = "My Projects";
+    currentProjectTitle.textContent = "This Project"; //placeholder for now
 
-        projectsListTitle.textContent = "Projects";
-        currentProjectTitle = "This Project"; //placeholder for now
+    projectListDiv.appendChild(projectsListTitle);
+    currentProjectDiv.appendChild(currentProjectTitle);
 
+    homeContainer.appendChild(projectListDiv);
+    homeContainer.appendChild(currentProjectDiv);
 
-
-    }
-
-    return {
-        createHomePage,
-    }
+    return homeContainer;
 
 }
 
-export { ScreenController };
+const addProjectToList = () => {
+
+    const projectListDiv = document.querySelector('#proj-list-div');
+
+    const project = ProjectManager();
+
+    const projArray = project.getMyProjects();
+
+    projArray.forEach((proj) => {
+
+        const projBtn = document.createElement('button');
+        projBtn.classList.add('project-button');
+        projBtn.textContent = `${proj.title}`
+        projectListDiv.appendChild(projBtn);
+
+    })
+
+    return projectListDiv;
+
+}
+
+const createThisProjectSection = () => {
+
+    const currentProjectDiv = document.querySelector('#current-proj-div');
+
+
+
+}
+
+
+
+
+export { 
+    createHomePage,
+    addProjectToList,
+    createThisProjectSection,
+ };
