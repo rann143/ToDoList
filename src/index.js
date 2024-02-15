b75b31da326830e5a77d54be2c5ed533ae3921ef
Daniel <roderm99@gmail.com>
2024-02-15 08:10:57 -0500

    add functionality where clicking on a project button clears the project display and shows selected project's title; still need to show todos

--- src/index.js
+++ src/index.js
@@ -21,9 +21,46 @@ addProjectToList();
 createAddNewToDoButton();
 displayProjectTodos();
 
-// Functionality where if click a project button in the list of projects,
+// Functionality where if I click a project button in the list of projects,
 // the display of the current project div changes to that respective project
 // and shows it's to-dos
+
+const projBtns = document.querySelectorAll('.project-button');
+const currentProjectDiv = document.querySelector('#current-proj-div');
+
+
+// put this in screendisplay.js
+function clearProjDivChildren() {
+
+    while (currentProjectDiv.firstChild) {
+        currentProjectDiv.removeChild(currentProjectDiv.lastChild);
+    }
+
+}
+
+projBtns.forEach((btn) => {
+
+    btn.addEventListener('click', event => {
+
+        // remove contents to make space for clicked on project
+        clearProjDivChildren();
+
+        // display title of clicked on project
+        const projTitle = document.createElement('h2');
+        projTitle.textContent = `${btn.textContent}`;
+        currentProjectDiv.appendChild(projTitle);
+
+        //display todo list for that project
+        
+
+    
+    });
+
+})
+
+
+
+
 
 
 
